429fa9e70faba249c0679e6527eeef4a90332bf0
Qiyu Li <0x9357@gmail.com>
2016-11-08 17:17:28 +0800

    improve api

--- README.md
+++ README.md
@@ -19,16 +19,16 @@ npm i JSONRpc --save
 ```javascript
 var JSONRpcClient = require('JSONRpc')
 
-let client = new JSONRpcClient('localhost', 8080)
+const client = new JSONRpcClient('localhost', 8080)
 
-client.Call('add', [1,7], (err, ret) => {
+client.call('add', [1,7], (err, ret) => {
     // 8
     if(!err) console.log(ret)
 })
 
 // Promise also support
 (async () => {
-    let ret = await client.CallPromise('add', [1,2])
+    let ret = await client.callPromise('add', [1,2])
     // 3
     console.log(ret)
 })()
--- test/test.js
+++ test/test.js
@@ -5,25 +5,25 @@ let client = new JSONRpcClient('localhost', 8080)
 
 function thunkPromise(method, params) {
     return new Promise(function (resolve, reject) {
-        client.Call(method, params, (err, ret) => {
+        client.call(method, params, (err, ret) => {
             if (err) reject(err)
             else resolve(ret)
         })
     })
 }
 
-test('add correct (Call)', async t => {
+test('add correct (call)', async t => {
     t.is(await thunkPromise('add', [1, 7]), 8)
 })
 
-test('add error (Call)', async t => {
+test('add error (call)', async t => {
     await thunkPromise('add', [1]).catch(err => t.is(err, 'error'))
 })
 
-test('add correct (CallPromise)', async t => {
-    t.is(await client.CallPromise('add', [1, 2]), 3)
+test('add correct (callPromise)', async t => {
+    t.is(await client.callPromise('add', [1, 2]), 3)
 })
 
-test('add error (CallPromise)', async t => {
-    await client.CallPromise('add', []).catch(err => t.is(err, 'error'))
+test('add error (callPromise)', async t => {
+    await client.callPromise('add', []).catch(err => t.is(err, 'error'))
 })
